b3eb9c281ad339a320b308064a867672c535e94e
mahdi <mehdilakhoua123@gmail.com>
2026-07-15 11:41:05 +0100

    Fixed format problems

--- src/api/users.ts
+++ src/api/users.ts
@@ -35,7 +35,7 @@ export async function handlerUsersGet(req: Request, res: Response, user: User) {
 
 function generateRandomSHA256Hash(): string {
   return crypto
-      .createHash("sha256")
-      .update(crypto.randomBytes(32))
-      .digest("hex");
+    .createHash("sha256")
+    .update(crypto.randomBytes(32))
+    .digest("hex");
 }
